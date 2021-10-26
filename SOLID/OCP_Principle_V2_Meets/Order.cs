4fe974e0a6ef446a72ddced5eacd459f593a4704
ivanclay <ivan.clay.moura@gmail.com>
2021-10-26 17:00:14 -0300

    OCP V2 Meets

---
+++ SOLID/OCP_Principle_V2_Meets/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCP_Principle_V2_Meets
+{
+    class Order
+    {
+        public virtual double OrderDiscount(double value)
+        {
+            return value;
+        }
+    }
+}
---
+++ SOLID/OCP_Principle_V2_Meets/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OCP_Principle_V2_Meets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order();
+
+            var commonConsumerOrderValue =
+               order.OrderDiscount(1000, ConsumerType.Common);
+
+            var associateConsumerOrderValue =
+                order.OrderDiscount(1000, ConsumerType.Associate);
+
+            var specialConsumerOrderValue =
+                order.OrderDiscount(1000, ConsumerType.Special);
+
+            Console.WriteLine("OCP Principle - Violation");
+
+            Console.WriteLine($"Common Consumer = " +
+                $"{commonConsumerOrderValue}");
+
+            Console.WriteLine($"Associate Consumer = " +
+                $"{associateConsumerOrderValue}");
+
+            Console.WriteLine($"Special Consumer = " +
+                $" {specialConsumerOrderValue}");
+
+            Console.ReadLine();
+        }
+    }
+}
