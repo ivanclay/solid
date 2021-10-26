4b9d6b3c66781dab499370be4c9eb122664e34ed
ivanclay <ivan.clay.moura@gmail.com>
2021-10-26 16:40:17 -0300

    OCP V2 violation

---
+++ SOLID/OCP_Principle_V2_Violation/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCP_Principle_V2_Violation
+{
+    public enum ConsumerType
+    {
+        Common,
+        Associate,
+        Special
+    };
+
+    class Order
+    {
+        public double OrderDiscount(double value, ConsumerType consumerType)
+        {
+            double finalValue;
+
+            if (consumerType == ConsumerType.Special)
+            {
+                finalValue = value - 50;
+            }
+            else if (consumerType == ConsumerType.Associate)
+            {
+                finalValue = value - 10;
+            }
+            else
+            {
+                finalValue = value;
+            }
+            return finalValue;
+        }
+    }
+}
--- SOLID/OCP_Principle_V2_Violation/Program.cs
+++ SOLID/OCP_Principle_V2_Violation/Program.cs
@@ -6,7 +6,29 @@ namespace OCP_Principle_V2_Violation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Order order = new Order();
+
+            var commonConsumerOrderValue =
+               order.OrderDiscount(1000, ConsumerType.Common);
+
+            var associateConsumerOrderValue =
+                order.OrderDiscount(1000, ConsumerType.Associate);
+
+            var specialConsumerOrderValue =
+                order.OrderDiscount(1000, ConsumerType.Special);
+
+            Console.WriteLine("OCP Principle - Violation");
+
+            Console.WriteLine($"Common Consumer = " +
+                $"{commonConsumerOrderValue}");
+
+            Console.WriteLine($"Associate Consumer = " +
+                $"{associateConsumerOrderValue}");
+
+            Console.WriteLine($"Special Consumer = " +
+                $" {specialConsumerOrderValue}");
+
+            Console.ReadLine();
         }
     }
 }
