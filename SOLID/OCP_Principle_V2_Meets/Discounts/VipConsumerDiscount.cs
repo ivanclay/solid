43e8aa88d4e15dab18b608ac504b4f9b43a18c4b
ivanclay <ivan.clay.moura@gmail.com>
2021-10-26 17:06:52 -0300

    OCP V2 Meets

---
+++ SOLID/OCP_Principle_V2_Meets/Discounts/VipConsumerDiscount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCP_Principle_V2_Meets.Discounts
+{
+    class VipConsumerDiscount : Order
+    {
+        public override double OrderDiscount(double value)
+        {
+            return value - 100;
+        }
+    }
+}
