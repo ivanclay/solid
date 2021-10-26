4e26f0f7a53de409ddb7295f417bd9750b287ec4
ivanclay <ivan.clay.moura@gmail.com>
2021-10-26 16:29:03 -0300

    OCP V2

---
+++ SOLID/OCP_Principle_V2_Violation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OCP_Principle_V2_Violation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
